3f6224bed4372cf94ecbb06c901afaaca6340c76
ORN <b.blanding90@yahoo.com>
2023-11-05 19:17:07 -0500

    Job Board App

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ["babel-preset-expo", 'module:metro-react-native-babel-preset'],
     plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "react-native-dotenv",
+          path: ".env",
+        },
+      ],
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
